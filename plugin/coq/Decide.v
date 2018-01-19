7dd0d68dcbcd94b421e5815dcb193d3ffdf406de
Nate Yazdani <nyazdani@cs.washington.edu>
2018-01-18 20:20:33 -0800

    Remove comment on now-resolved questions

--- plugin/coq/Decide.v
+++ plugin/coq/Decide.v
@@ -7,10 +7,3 @@ Require Import Patcher.Patch.
 Register Patch Tactic (intros; omega) as omega for Z Z.lt Z.gt Z.le Z.ge (@eq Z).
 
 Decide (forall x : Z, x - 1 < x) with omega as proof.
-
-(* Some observations:
- *   1. Some tactics, like Omega, need all hypotheses to be introduced before
- *      invocation.
- *   2. The robustness of the internal representation of a tactic script is.
- *      unclear. In particular, does it close over the tactic environment?
- *)
