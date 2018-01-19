1bca1738823211dc2badcbdb465fb03af40fcb57
Nate Yazdani <nyazdani@cs.washington.edu>
2018-01-18 17:22:35 -0800

    Fix some typos

--- plugin/coq/Decide.v
+++ plugin/coq/Decide.v
@@ -4,12 +4,12 @@ Open Scope Z_scope.
 
 Require Import Patcher.Patch.
 
-(* Super simple text of the Decide command: *)
+(* Super simple test of the Decide command: *)
 Decide (forall x : Z, x - 1 < x) with (intros; omega) as proof.
 
 (* Some observations:
  *   1. Some tactics, like Omega, need all hypotheses to be introduced before
  *      invocation.
  *   2. The robustness of the internal representation of a tactic script is.
- *      In particular, does it close over the tactic environment?
+ *      unclear. In particular, does it close over the tactic environment?
  *)
