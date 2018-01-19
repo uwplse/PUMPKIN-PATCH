9fbb5aed26073b7eda1c23814a8aed463eba9f3a
Nate Yazdani <nyazdani@cs.washington.edu>
2018-01-18 19:35:09 -0800

    Separate test case for tactic registration

---
+++ plugin/coq/Register.v
@@ -0,0 +1,8 @@
+Require Import Coq.omega.Omega.
+Require Import Coq.ZArith.ZArith.
+Open Scope Z_scope.
+
+Require Import Patcher.Patch.
+
+Register Tactic (intros; omega) as omega for Z Z.lt Z.gt Z.le Z.ge (@eq Z).
+Unregister Tactic omega.
