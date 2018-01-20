9e0f029d2fcff59a42f458cf13053a0c4d6b9c46
Nate Yazdani <nyazdani@cs.washington.edu>
2018-01-20 09:58:13 -0800

    Tidy up configuration for Merlin

--- plugin/merlin.sh
+++ plugin/merlin.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+
+OPAM="${HOME}/.opam/$(opam switch show)"
+
+cat >.merlin <<EOF
 FLG -rectypes -thread -w @1..50@59-4-44 -safe-string
 
 S src/**
@@ -5,3 +10,6 @@ B src/**
 
 PKG threads threads.posix
 PKG coq coq.config coq.engine coq.grammar coq.highparsing coq.interp coq.intf coq.kernel coq.lib coq.library coq.ltac coq.parsing coq.pretyping coq.printing coq.proofs coq.stm coq.tactics coq.toplevel coq.vm
+S ${OPAM}/build/coq.8.6/**
+CMT ${OPAM}/build/coq.8.6/**
+EOF
