c0a566c49726b3a8a4690728af6f2a675697c275
Talia Ringer <tringer@cs.washington.edu>
2019-05-18 20:44:32 -0400

    add another one of Nate's functions

--- plugin/src/library/proofsearch/preprocessing/desugar.ml
+++ plugin/src/library/proofsearch/preprocessing/desugar.ml
@@ -14,7 +14,7 @@ open CErrors
 open Coqterms
 open Abstraction
 
-(* --- Begin declaration management --- *)
+(* --- Begin DEVOID library code --- *)
        
 (*
  * When we merge PUMPKIN with DEVOID, these will move into
@@ -48,7 +48,24 @@ let rel_value decl =
 let rel_type decl =
   Rel.Declaration.get_type decl
 
-(* --- End declaration management --- *)
+(* Is the first term equal to a "head" (application prefix) of the second?
+ * The notion of term equality is syntactic (i.e., no environment) and defaults
+ * to syntactic equality modulo alpha, casts, grouping, and universes. The
+ * result of this function is an informative boolean: an optional array, with
+ * None meaning false and Some meaning true and giving the trailing arguments.
+ *
+ * This function is similar to is_or_applies, except for term equality and the
+ * informative boolean result.
+ *)
+let eq_constr_head ?(eq_constr=eq_constr_nounivs) term term' =
+  let head, args = decompose_app term in
+  let head', args' = decompose_app term' in
+  if eq_constr head head' && List.prefix_of eq_constr args args' then
+    Some (List.skipn (List.length args) args' |> Array.of_list)
+  else
+    None
+                           
+(* --- End DEVOID library code --- *)
 
 (*
  * Pair the outputs of two functions on the same input.
