dddef19484a4e45fecc55c1c3edf5e05fe63e636
Talia Ringer <tringer@cs.washington.edu>
2019-08-09 10:31:27 -0700

    Fix one of the two abstraction bugs

--- plugin/src/core/components/abstraction/abstracters.ml
+++ plugin/src/core/components/abstraction/abstracters.ml
@@ -108,6 +108,18 @@ let types_full env evd (arg_actual : types) (arg_abstract : types) (trms : candi
 let types_full_strategy : abstracter =
   types_full
 
+(* A pattern-based full abstraction strategy for functions *)
+(* TODO really just need a more flexible top-level function that lets you combine strategies *)
+let function_pattern_full (env : env) (evd : evar_map) (arg_actual : types) (arg_abstract : types) (trms : types list) : types list =
+  match kind arg_abstract with
+  | App (f, args) ->
+     syntactic_full env evd arg_actual arg_abstract trms
+  | _ ->
+     types_full env evd arg_actual arg_abstract trms
+
+let function_pattern_full_strategy : abstracter =
+  function_pattern_full
+
 (* A pattern-based full abstraction strategy for constructors *)
 let pattern_full (env : env) (evd : evar_map) (arg_actual : types) (arg_abstract : types) (trms : types list) : types list =
   let types_conv = types_convertible env evd arg_abstract in
@@ -176,6 +188,27 @@ let types_full_no_reduce : abstraction_strategy =
 
 (*
  * Reduce first
+ * Replace functions with abstracted functions when types are convertible
+ * Replace applications with abstracted applications when terms are convertible 
+ *)
+let function_pattern_full_reduce : abstraction_strategy =
+  {
+    reducer = reduce_remove_identities;
+    abstracter = function_pattern_full_strategy;
+    filter = filter_by_type;
+    to_abstract = Arguments;
+  }
+
+(*
+ * Don't reduce
+ * Otherwise act like function_pattern_no_reduce
+ *)
+let function_pattern_full_no_reduce : abstraction_strategy =
+  { function_pattern_full_reduce with reducer = remove_identities; }
+
+    
+(*
+ * Reduce first
  * Replace all terms matching a pattern (f, args) with abstracted terms
  * Fall back to syntactic_full when the concrete argument is not a pattern
  *)
@@ -241,17 +274,17 @@ let simple_strategies : abstraction_strategy list =
 
 (* --- Strategies for abstracting properties --- *)
 
-let types_full_reduce_prop : abstraction_strategy =
-  { types_full_reduce with to_abstract = Property }
+let function_pattern_full_reduce_prop : abstraction_strategy =
+  { function_pattern_full_reduce with to_abstract = Property }
 
-let types_full_no_reduce_prop : abstraction_strategy =
-  { types_full_no_reduce with to_abstract = Property }
+let function_pattern_full_no_reduce_prop : abstraction_strategy =
+  { function_pattern_full_no_reduce with to_abstract = Property }
 
 let reduce_strategies_prop : abstraction_strategy list =
-  [types_full_reduce_prop]
+  [function_pattern_full_reduce_prop]
 
 let no_reduce_strategies_prop : abstraction_strategy list =
-  [types_full_no_reduce_prop]
+  [function_pattern_full_no_reduce_prop]
 
 let default_strategies_prop : abstraction_strategy list =
   List.append reduce_strategies_prop no_reduce_strategies_prop
