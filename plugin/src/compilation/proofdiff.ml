5853874ed1b2cd649846c59887e4cec7cfd70c92
Talia Ringer <tringer@cs.washington.edu>
2019-09-17 15:26:35 -0700

    tweak

--- plugin/src/compilation/proofdiff.ml
+++ plugin/src/compilation/proofdiff.ml
@@ -256,12 +256,12 @@ let update_case_assums (ms_o, ms_n, assums) =
   fold_left_state
     (fun assums (dst_o, dst_n) ->
       let d = dst_o, dst_n, assums in
-      let (env, (o, n, _), _) = merge_lift_diff_envs d [] in
+      let (env, d_goal, _) = merge_lift_diff_envs d [] in
       branch_state
 	(fun (o, n, _) sigma -> convertible env sigma o n)
 	(fun _ -> ret (assume_local_equal assums))
 	(fun _ -> ret (shift_assumptions assums))
-	(o, n, assums))
+	d_goal)
     assums
     (List.fold_right2
        (fun dst_o dst_n combined -> (dst_o, dst_n) :: combined)
