6b8a19bb1314591b2d7546176d534c8610269b7f
RanDair Porter <randair@uw.edu>
2021-08-17 09:46:03 -0700

    Decompile all modules from fundamental-arithmetics

---
+++ plugin/coq/FundamentalArithmetics.v
@@ -0,0 +1,16 @@
+Add LoadPath "./deps/fundamental-arithmetics" as FundamentalArithmetics.
+From FundamentalArithmetics Require Import 
+  division euclide gcd missing nthroot permutation
+  power primes.
+Require Import Patcher.Patch.
+
+
+Decompile Module division.
+Decompile Module euclide.
+Decompile Module gcd.
+Decompile Module missing.
+Decompile Module nthroot.
+Decompile Module permutation.
+Decompile Module power.
+Decompile Module primes.
+
